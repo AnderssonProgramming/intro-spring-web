ae9b883fee325e8361c2a7cec1b6ef79cd6c88f6
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-26 11:43:12 -0500

    feat: add hello post method into controller and add html and js reload requirements

--- src/main/java/edu/eci/arsw/introSpring/WebSiteController.java
+++ src/main/java/edu/eci/arsw/introSpring/WebSiteController.java
@@ -3,6 +3,7 @@ package edu.eci.arsw.introSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,5 +27,10 @@ public class WebSiteController {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s! (via POST)", name);
     }
 }
